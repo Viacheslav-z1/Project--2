317d7c04e0d62d8c33b313701953793c490d372d
Viacheslav-z1 <93464902+Viacheslav-z1@users.noreply.github.com>
2021-12-21 16:41:55 +0200

    tol

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+ 
+
+    $('.carousel__inner').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
